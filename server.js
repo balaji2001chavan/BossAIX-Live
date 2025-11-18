fd17cd23a1396155bf1cb14382130f159e2d7317
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-11-18 22:19:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Serve everything in ROOT directory
 app.use(express.static(__dirname));
 
 app.get("*", (req, res) => {
